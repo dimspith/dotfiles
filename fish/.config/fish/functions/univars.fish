932677079ee9b0f64a0fea54adc748553b6c7144
dimitrissp <risorg@tuta.io>
2020-02-05 18:00:50 +0200

    Fix variable setting

--- fish/.config/fish/functions/univars.fish
+++ fish/.config/fish/functions/univars.fish
@@ -1,31 +1,35 @@
 function univars --description "Set universal variables"
     # GTK and QT settings
-    set -U QT_QPA_PLATFORMTHEME "qt5ct"
-    set -U GTK2_RC_FILES "$HOME/.gtkrc-2.0"
+    set -Ux QT_QPA_PLATFORMTHEME "qt5ct"
+    set -Ux GTK2_RC_FILES "$HOME/.gtkrc-2.0"
 
     # Default application values
-    set -U EDITOR "emacsclient -c -nw"
-    set -U BROWSER "firefox"
-    set -U TERMINAL "alacritty"
-    set -U PASSWORD_STORE_ENABLE_EXTENSIONS true
+    set -Ux EDITOR "emacsclient -c -nw"
+    set -Ux BROWSER "firefox"
+    set -Ux TERMINAL "alacritty"
+    set -gx PASSWORD_STORE_ENABLE_EXTENSIONS "true"
     set -gx TERM "xterm-256color"
 
     # Set the default configuration file folder
     set -U XDG_CONFIG_HOME "$HOME/.config"
 
     #### PATH ####
-    # ZSH path fix
-    set -gx PATH $HOME/bin:/usr/local/bin $PATH
     # Ruby binaries
-    set -gx PATH $HOME/.gem/ruby/2.6.0/bin $PATH
+    set -U fish_user_paths $HOME/.gem/ruby/2.6.0/bin $fish_user_paths
+    # set -gx PATH $HOME/.gem/ruby/2.6.0/bin $PATH
     # Haskell binaries
-    set -gx PATH $HOME/.cabal/bin $PATH
+    set -U fish_user_paths $HOME/.cabal/bin $fish_user_paths
+    # set -gx PATH $HOME/.cabal/bin $PATH
     # Rust binaries
-    set -gx PATH $HOME/.cargo/bin $PATH
+    set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
+    # set -gx PATH $HOME/.cargo/bin $PATH
     # Local binaries
-    set -gx PATH $HOME/.local/bin $PATH
+    set -U fish_user_paths $HOME/.local/bin $fish_user_paths
+    # set -gx PATH $HOME/.local/bin $PATH
     # npm
-    set -gx PATH $HOME/.npm-global/bin $PATH
+    set -U fish_user_paths $HOME/.npm-global/bin $fish_user_paths
+    # set -gx PATH $HOME/.npm-global/bin $PATH
     # go
-    set -gx PATH $HOME/MyPrograms/go/bin $PATH
+    set -U fish_user_paths $HOME/MyPrograms/go/bin $fish_user_paths
+    # set -gx PATH $HOME/MyPrograms/go/bin $PATH
 end
