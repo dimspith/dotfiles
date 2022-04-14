3cb55e9b6d9656ec8aaba73395a1c519adc18544
dimspith <51262483+dimspith@users.noreply.github.com>
2022-04-14 19:05:19 +0300

    Changes

--- awesomewm/.config/awesome/config/startup.lua
+++ awesomewm/.config/awesome/config/startup.lua
@@ -8,15 +8,15 @@ local function run_once(cmd_arr)
 end
 
 run_once({
-      "pipewire",
-      "wireplumber",
-      "pipewire-pulse",
-      "emacs --daemon",
+      -- "pipewire",
+      -- "wireplumber",
+      -- "pipewire-pulse",
+      -- "emacs --daemon",
       "picom",
       "pasystray",
       "nm-applet",
-      "udiskie",
-      "setxkbmap -model pc105 -layout us,gr -option grp:rctrl_toggle -option ctrl:nocaps",
-      "xset r rate 260 32",
+      -- "udiskie",
+      -- "setxkbmap -model pc105 -layout us,gr -option grp:rctrl_toggle -option ctrl:nocaps",
+      -- "xset r rate 260 32",
       "/usr/libexec/polkit-gnome-authentication-agent-1"
 })
